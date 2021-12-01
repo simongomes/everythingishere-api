e758787fc60b2b5f6360a268ab0b371fd32f5b2d
Simon Gomes <simon.gomes@umma.id>
2021-12-01 13:52:20 +0600

    code updated for new deployment

--- config/constants.js
+++ config/constants.js
@@ -1,1 +1,3 @@
-exports.API_ENDPOINT = 'https://www.reader.one/api/';
+exports.API_ENDPOINT = "https://www.reader.one/api/";
+exports.API_ENDPOINT_GITHUB =
+  "https://gh-trending-api.herokuapp.com/developers";
--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,9 +1,9 @@
-const axios = require('axios');
-const { API_ENDPOINT } = require('../config/constants');
+const axios = require("axios");
+const { API_ENDPOINT, API_ENDPOINT_GITHUB } = require("../config/constants");
 
 exports.index = (req, res, next) => {
   res.json(
-    'Welcome to Everything is here API. It provides necessery data for https://everythingishere.top'
+    "Welcome to Everything is here API. It provides necessery data for https://everythingishere.top"
   );
 };
 
@@ -11,14 +11,22 @@ exports.index = (req, res, next) => {
 exports.getStoriesByChannel = (req, res, next) => {
   const { channel } = req.params;
   const { limit } = req.query;
-  const URL = `${API_ENDPOINT}news/${channel}?limit=${limit ? limit : 20}`;
+  let URL = `${API_ENDPOINT}news/${channel}?limit=${limit ? limit : 20}`;
+
+  if (channel === "github") {
+    URL = `${API_ENDPOINT_GITHUB}?limit=${limit ? limit : 20}?`;
+  }
   axios
     .get(URL)
-    .then(response => {
-      res.json(response.data);
+    .then((response) => {
+      if (channel === "github") {
+        res.json(mapGitHubTrends(response.data));
+      } else {
+        res.json(response.data);
+      }
     })
-    .catch(function(error) {
-      console.log('Error ' + error.message);
+    .catch(function (error) {
+      console.log("Error " + error.message);
     });
 };
 
@@ -29,10 +37,21 @@ exports.getAllStories = (req, res, next) => {
   const URL = `${API_ENDPOINT}all/${channels}?limit=${limit ? limit : 20}`;
   axios
     .get(URL)
-    .then(response => {
+    .then((response) => {
       res.json(response.data);
     })
-    .catch(function(error) {
-      console.log('Error ' + error.message);
+    .catch(function (error) {
+      console.log("Error " + error.message);
     });
+};
+
+const mapGitHubTrends = (results) => {
+  return results.map((item) => {
+    return {
+      author: item.name,
+      title: item.popularRepository.repositoryName,
+      url: item.url,
+      score: item.rank,
+    };
+  });
 };
